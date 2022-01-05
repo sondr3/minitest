54f05d4e816618f0589a374e11eebff903ee305e
Sondre Nilsen <sondre@eons.io>
2022-01-05 15:10:59 +0100

    Set eslint envs correctly, engines in package.json

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,14 +1,13 @@
 /** @type {import('eslint').Linter.Config} */
 module.exports = {
   env: {
-    browser: true,
+    node: true,
     es2021: true,
   },
   extends: ["@sondr3/eslint-config/typescript"],
   parser: "@typescript-eslint/parser",
   parserOptions: {
     project: "tsconfig.json",
-    ecmaVersion: 12,
     sourceType: "module",
   },
   rules: {
