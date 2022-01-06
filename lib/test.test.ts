11099c6a65786115802219a6e4034d237823d060
Sondre Nilsen <sondre@eons.io>
2022-01-06 11:39:37 +0100

    Add some tests, refactor to use TestRunner

--- lib/test.test.ts
+++ lib/test.test.ts
@@ -1,8 +1,34 @@
+// Disable this rule for this file to test erroneous usage of `test`
+/* eslint-disable @typescript-eslint/ban-ts-comment */
+
 import { strict as assert } from "node:assert";
 
 import { test } from "./index.js";
 
 // yo, i heard you like tests, so i put tests in your tests
+
+test("ignore() works", () => {
+  const it = test("internal", () => {
+    // no-op
+  });
+  it.ignore();
+  const runner = it.toTestRunner();
+
+  assert(runner.ignore);
+  assert.equal(runner.name, "internal");
+});
+
+test("only() works", () => {
+  const it = test("only internal", () => {
+    // no-op
+  });
+  it.only();
+  const runner = it.toTestRunner();
+
+  assert(!runner.ignore);
+  assert(runner.only);
+  assert.equal(runner.name, "only internal");
+});
 
 test("object only", () => {
   const it = test({
@@ -10,7 +36,7 @@ test("object only", () => {
     fn: () => {
       /* */
     },
-  });
+  }).toTestRunner();
 
   assert.equal(it.name, "has name");
 });
@@ -46,13 +72,13 @@ test("object with two fn throws", () => {
 test("anonymous function only", () => {
   const it = test(() => {
     /* */
-  });
+  }).toTestRunner();
   assert.equal(it.name, "unnamed");
 });
 
 test("named function only", () => {
   const it = test(function thisHasAName() {
     /* */
-  });
+  }).toTestRunner();
   assert.equal(it.name, "thisHasAName");
 });
